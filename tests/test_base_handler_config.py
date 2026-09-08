79e2d257d14f87a0cedfd4be146e7a8e99c074ce
zhangdaodragon <dlzhangak@isoftstone.com>
2026-09-08 15:12:42 +0800

    test: add unit tests for base_handler_config.py
    - Test read_json with list and empty list
    - Test conf_compare for equal, different, missing item
    - Test init defaults

---
+++ tests/test_base_handler_config.py
@@ -0,0 +1,66 @@
+# ******************************************************************************
+# Copyright (c) Huawei Technologies Co., Ltd. 2021-2022. All rights reserved.
+# licensed under the Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#     http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN 'AS IS' BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+# PURPOSE.
+# See the Mulan PSL v2 for more details.
+# ******************************************************************************/
+"""
+Description: unit tests for base_handler_config.py
+"""
+import json
+import unittest
+
+from ragdoll.app.config_model.base_handler_config import BaseHandlerConfig
+from ragdoll.app.constant import SYNCHRONIZED, NOT_SYNCHRONIZE
+
+
+class TestBaseHandlerConfig(unittest.TestCase):
+    """Tests for BaseHandlerConfig class."""
+
+    def setUp(self):
+        self.config = BaseHandlerConfig()
+
+    def test_read_json_list(self):
+        """Test read_json with a JSON list."""
+        data = [{"key": "val1"}, {"key": "val2"}]
+        self.config.read_json("/tmp/fake", json.dumps(data))
+        self.assertEqual(len(self.config.conf), 2)
+        self.assertEqual(self.config.conf[0]["key"], "val1")
+
+    def test_read_json_empty_list(self):
+        """Test read_json with an empty JSON list."""
+        self.config.read_json("/tmp/fake", "[]")
+        self.assertEqual(self.config.conf, [])
+
+    def test_conf_compare_equal(self):
+        """Test conf_compare returns SYNCHRONIZED when lists are equal."""
+        c1 = json.dumps([{"a": "1"}, {"b": "2"}])
+        c2 = json.dumps([{"a": "1"}, {"b": "2"}])
+        self.assertEqual(self.config.conf_compare(c1, c2), SYNCHRONIZED)
+
+    def test_conf_compare_different(self):
+        """Test conf_compare returns NOT_SYNCHRONIZE when values differ."""
+        c1 = json.dumps([{"a": "1"}])
+        c2 = json.dumps([{"a": "2"}])
+        self.assertEqual(self.config.conf_compare(c1, c2), NOT_SYNCHRONIZE)
+
+    def test_conf_compare_missing_item(self):
+        """Test conf_compare returns NOT_SYNCHRONIZE when item is missing."""
+        c1 = json.dumps([{"a": "1"}, {"b": "2"}])
+        c2 = json.dumps([{"a": "1"}])
+        self.assertEqual(self.config.conf_compare(c1, c2), NOT_SYNCHRONIZE)
+
+    def test_init_defaults(self):
+        """Test BaseHandlerConfig initializes with empty lists."""
+        cfg = BaseHandlerConfig()
+        self.assertEqual(cfg.conf, [])
+        self.assertEqual(cfg.yang, [])
+
+
+if __name__ == "__main__":
+    unittest.main()
