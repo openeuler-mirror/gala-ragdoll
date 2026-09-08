5421467302dd537455454ebcdf64dff28681b10a
zhangdaodragon <dlzhangak@isoftstone.com>
2026-09-08 16:44:01 +0800

    test: add unit tests for base_handler_config.py

--- tests/test_base_handler_config.py
+++ tests/test_base_handler_config.py
@@ -18,6 +18,13 @@ import unittest
 from ragdoll.app.config_model.base_handler_config import BaseHandlerConfig
 from ragdoll.app.constant import SYNCHRONIZED, NOT_SYNCHRONIZE
 
+FAKE_PATH = "/tmp/fake"
+KEY = "key"
+KEY_A = "a"
+KEY_B = "b"
+VAL_1 = "1"
+VAL_2 = "2"
+
 
 class TestBaseHandlerConfig(unittest.TestCase):
     """Tests for BaseHandlerConfig class."""
@@ -27,32 +34,32 @@ class TestBaseHandlerConfig(unittest.TestCase):
 
     def test_read_json_list(self):
         """Test read_json with a JSON list."""
-        data = [{"key": "val1"}, {"key": "val2"}]
-        self.config.read_json("/tmp/fake", json.dumps(data))
+        data = [{KEY: "val1"}, {KEY: "val2"}]
+        self.config.read_json(FAKE_PATH, json.dumps(data))
         self.assertEqual(len(self.config.conf), 2)
-        self.assertEqual(self.config.conf[0]["key"], "val1")
+        self.assertEqual(self.config.conf[0][KEY], "val1")
 
     def test_read_json_empty_list(self):
         """Test read_json with an empty JSON list."""
-        self.config.read_json("/tmp/fake", "[]")
+        self.config.read_json(FAKE_PATH, "[]")
         self.assertEqual(self.config.conf, [])
 
     def test_conf_compare_equal(self):
         """Test conf_compare returns SYNCHRONIZED when lists are equal."""
-        c1 = json.dumps([{"a": "1"}, {"b": "2"}])
-        c2 = json.dumps([{"a": "1"}, {"b": "2"}])
+        c1 = json.dumps([{KEY_A: VAL_1}, {KEY_B: VAL_2}])
+        c2 = json.dumps([{KEY_A: VAL_1}, {KEY_B: VAL_2}])
         self.assertEqual(self.config.conf_compare(c1, c2), SYNCHRONIZED)
 
     def test_conf_compare_different(self):
         """Test conf_compare returns NOT_SYNCHRONIZE when values differ."""
-        c1 = json.dumps([{"a": "1"}])
-        c2 = json.dumps([{"a": "2"}])
+        c1 = json.dumps([{KEY_A: VAL_1}])
+        c2 = json.dumps([{KEY_A: VAL_2}])
         self.assertEqual(self.config.conf_compare(c1, c2), NOT_SYNCHRONIZE)
 
     def test_conf_compare_missing_item(self):
         """Test conf_compare returns NOT_SYNCHRONIZE when item is missing."""
-        c1 = json.dumps([{"a": "1"}, {"b": "2"}])
-        c2 = json.dumps([{"a": "1"}])
+        c1 = json.dumps([{KEY_A: VAL_1}, {KEY_B: VAL_2}])
+        c2 = json.dumps([{KEY_A: VAL_1}])
         self.assertEqual(self.config.conf_compare(c1, c2), NOT_SYNCHRONIZE)
 
     def test_init_defaults(self):
